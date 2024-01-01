1416859385f2d4c36a074ebde23f2670885ff2d8
zhuyf <zhuyifei.phil@gmial.com>
2024-01-01 15:14:22 +0800

    v1.0

---
+++ get_Gaussian16_props.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sub_code_get_GS_opt_props import main_import as get_GS_props
+from sub_code_get_ES_opt_props import main_import as get_ES_props
+
+
+def get_GS_props_from_log(input_file, save_file,state,work_type):
+    log_file=f'{input_file}'
+    prop_file=f'{save_file}'
+    state_to_function = {
+                        'GS': get_GS_props,
+                        'ES': get_ES_props
+                        }
+    state_to_function[state](log_file, prop_file, work_type)
+    print('Done!')
+
+if __name__ == '__main__':
+
+    print('Done!')
